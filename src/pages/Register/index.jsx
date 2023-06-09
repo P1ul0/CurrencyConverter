1f7220ed579a43bc7b3c880083bafb8902134008
paulo <paulocesar147852@gmail.com>
2023-06-09 13:19:54 -0300

    fazendo o mesmo do commit anterior so que na tela de register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -4,12 +4,26 @@ import { FormCenter, DivRegister, Title } from "./style"
 import { ButtonRedirect } from "../../components/buttonRedirect"
 import { RegisterIn } from "../../schemas"
 import { useNavigate } from "react-router"
+import { useEffect, useState } from "react"
 
 export const Register = () => {
     const navigate = useNavigate();
     const form = RegisterIn();
+    const [activeButton, setActiveButton] = useState(false);
+    const username = form.watch("username")
+    const email = form.watch("email");
+    const password = form.watch("password");
+    const confirmPassword = form.watch("confirmPassword");
+
+    useEffect(() => {
+        if( username !== "" && username.length >=8 && email.includes("@gmail.com") && password.length >= 6 && confirmPassword.length >= 6 && password === confirmPassword){
+            setActiveButton(true);
+        }else{
+            setActiveButton(false);
+        }
+    },[form.watch("username"),form.watch("email") , form.watch("password"),form.watch("confirmPassword"),activeButton])
+
     const onSubmit = (data) => {
-        console.log(data);
         navigate("/")
     };
     return(
@@ -22,7 +36,7 @@ export const Register = () => {
                 <InputGlobal register={form.register} error={form.errors.password?.message} name="password" type="password" label="Password" id="5"/>
                 <InputGlobal register={form.register} error={form.errors.confirmPassword?.message} name="confirmPassword" type="password" label="Confirm Password" id="6"/>
                 <ButtonRedirect Text="Ja Possui Cadastro ?"  Rota="/"/>
-                <ButtonRgb   text="Registre-se"/>
+                <ButtonRgb text="Registre-se" status={activeButton}/>
             </FormCenter>
         </DivRegister>
         </>
